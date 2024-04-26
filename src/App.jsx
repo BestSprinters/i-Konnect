c19ba70e845f30d38a97838463d132879b67a74b
CitrusSoda <dndhkaus1004@naver.com>
2024-04-26 17:36:19 +0900

    🎉 프로젝트 시작!

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
