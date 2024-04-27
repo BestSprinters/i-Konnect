c83a812e23d7bbda902f78c9629748f4db917829
김보미 <kimbomi2172@naver.com>
2024-04-27 16:17:19 +0900

    📦 chore: 폰트 초기 설정
    - Pretandard 폰트 파일 추가
    - index.css에 폰트 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import './index.css';
 
 function App() {
   return (
-    <div className="flex items-center justify-center">
-      <h1>Hello World!</h1>
+    <div className="flex-row items-center justify-center">
+      <h1 className="font-pretendard font-bold-26">Hello World!!!!</h1>
+      <p className="fb26">안녕하세요.</p>
+      <p className="fb20">안녕하세요.</p>
+      <p className="fb20-l26">안녕하세요.</p>
+      <p className="fsb18">안녕하세요.</p>
+      <p className="fm18">안녕하세요.</p>
+      <p className="fb16-l26">안녕하세요.</p>
+      <p className="fb16-l26-ls5">안녕하세요.</p>
+      <p className="fm16">안녕하세요.</p>
+      <p className="fr16">안녕하세요.</p>
+      <p className="fr16-l18-ls-0_17">안녕하세요.</p>
+      <p className="fb15-l26">안녕하세요.</p>
+      <p className="fr14">안녕하세요.</p>
+      <p className="fm12">안녕하세요.</p>
+      <p className="fm12-l18-ls-0_17">안녕하세요.</p>
     </div>
   );
 }
