18939b0efb67ea81707e3b3f9bc4c5a18277f3bb
김보미 <kimbomi2172@naver.com>
2024-04-28 16:04:40 +0900

    🎨 style: 테일윈드 컬러 변수 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/button-has-type */
 import './index.css';
 
 function App() {
   return (
     <div className="flex items-center justify-center">
-      <h1>Hello World!</h1>
+      <h1 className="bg-grayBlue">Hello World!</h1>
+      <button className="bg-gradient-to-r from-pointOrange to-pointPink text-whitePrimary">
+        안녕하세요
+      </button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable no-unused-vars */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackPrimary: '#02000E',
+        blackSecondary: '#181D26',
+
+        yellowPrimary: '#D2C030',
+
+        grayLight: '#A3A5A8',
+        grayMedium: '#828282',
+        grayDark: '#67666E',
+        grayBlue: '#8C92AB',
+
+        whitePrimary: '#FFF',
+        whiteSecondary: '#F7F7F8',
+
+        pointOrange: '#F96D69',
+        pointPink: '#FE5493',
+      },
+    },
   },
   plugins: [],
 };
