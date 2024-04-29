46a74817e8d03e57f7acbb81edb325d33d8994e6
김보미 <kimbomi2172@naver.com>
2024-04-29 12:41:29 +0900

    🎨 style: 전역 폰트 재설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './index.css';
 function App() {
   return (
     <div className="container flex-row items-center justify-center">
-      <p className="text-xl font-bold">안녕하세요.</p>
+      <p className="font-bold">안녕하세요.</p>
+      <p className="font-semibold">안녕하세요.</p>
+      <p className="font-regular">안녕하세요.</p>
+      <p className="font-medium">안녕하세요.</p>
     </div>
   );
 }
