f97c01656ccde36aeeb49ceac675bd6ccae41eb3
Darkthos <kiki7951@naver.com>
2024-04-29 21:35:53 +0900

    📝 docs: apis폴더및 하위파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import getProducts from './apis/idolsApi';
 import './index.css';
 
 function App() {
+  getProducts();
   return (
     <div className="flex items-center justify-center">
       <h1>Hello World!</h1>
