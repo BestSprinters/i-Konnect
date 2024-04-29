3ae460476ee3c1074ba2d2533bd9bab9488cd737
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-04-29 14:06:23 +0900

    🎨 style: 테일윈드 컬러 변수 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
+/* eslint-disable react/button-has-type */
 import './index.css';
 
 function App() {
-  return (
-    <div className="container flex-row items-center justify-center">
-      <p className="font-bold">안녕하세요.</p>
-      <p className="font-semibold">안녕하세요.</p>
-      <p className="font-regular">안녕하세요.</p>
-      <p className="font-medium">안녕하세요.</p>
-    </div>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable no-unused-vars */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Pretendard'],
+      colors: {
+        blackPrimary: '#02000E',
+        blackSecondary: '#181D26',
+
+        yellowPrimary: '#D2C030',
+
+        grayLight: '#A3A5A8',
+        grayMedium: '#828282',
+        grayDark: '#67666E',
+        grayBlue: '#8C92AB',
+
+        whitePrimary: '#FFF',
+        whiteSecondary: '#F7F7F8',
+
+        pointOrange: '#F96D69',
+        pointPink: '#FE5493',
+        fontFamily: {
+          sans: ['Pretendard'],
+        },
       },
     },
   },
