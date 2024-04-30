8e6c884d1b8b0177e0664acbb001d0f9e724d8d1
김보미 <kimbomi2172@naver.com>
2024-04-30 16:57:10 +0900

    ✨ feat: 헤더 라우터 설정
    - 로고 클릭 시 /list로 이동
    - 프로필 클릭 시 /my로 이동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import imgLogo from '../assets/imgs/img_logo.png';
 import myProfile from '../assets/imgs/loopy.png';
@@ -6,15 +7,19 @@ import myProfile from '../assets/imgs/loopy.png';
 function Header() {
   return (
     <div className="fixed left-0 top-0 flex w-full items-center justify-center bg-blackPrimary">
-      <div className="container flex h-[80px] items-center justify-center">
-        <img src={imgLogo} alt="로고" className="w-[200px] cursor-pointer" />
+      <div className="justify-centfeater container flex h-[80px] items-center">
+        <Link to="/list">
+          <img src={imgLogo} alt="로고" className="w-[200px] cursor-pointer" />
+        </Link>
       </div>
       <div>
-        <img
-          src={myProfile}
-          alt="프로필"
-          className="float-right mr-4 h-[32px] w-[32px] cursor-pointer rounded-full"
-        />
+        <Link to="/my">
+          <img
+            src={myProfile}
+            alt="프로필"
+            className="float-right mr-4 h-[32px] w-[32px] cursor-pointer rounded-full"
+          />
+        </Link>
       </div>
     </div>
   );
