04e5b57e0a52b94b9d48ab12f246d9a9d2f7530d
kimhyesun <hyesun1004.kim@gmail.com>
2024-05-01 01:37:17 +0900

    ✨ Feat: idolThumbnail 생성
    myPage idolList에 들어갈 idolthumbnail 작업

---
+++ src/components/idolThumbnail.jsx
@@ -0,0 +1,12 @@
+function idolThumbnail(group, name) {
+  return (
+    <div>
+      <dl>
+        <dt>{group}</dt>
+        <dd>{name}</dd>
+      </dl>
+    </div>
+  );
+}
+
+export default idolThumbnail;
---
+++ src/pages/myPage.jsx
@@ -0,0 +1,5 @@
+function myPage() {
+  return <div>myPage</div>;
+}
+
+export default myPage;
