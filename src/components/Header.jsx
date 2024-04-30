90e3b77fcc581720b9337ed485005c06455f99fc
김보미 <kimbomi2172@naver.com>
2024-04-30 17:39:37 +0900

    ✨ feat: 스크롤 감지 시 헤더 배경 색 변화

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import imgLogo from '../assets/imgs/img_logo.png';
 import myProfile from '../assets/imgs/loopy.png';
 
 function Header() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const baseHeaderStyle =
+    'fixed left-0 top-0 flex w-full items-center justify-center';
+
+  const headerScrollStyle = {
+    base: 'bg-transparent',
+    scroll: 'bg-blackPrimary',
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerScrollValid =
+    scrollPosition === 0 ? headerScrollStyle.base : headerScrollStyle.scroll;
+
   return (
-    <div className="fixed left-0 top-0 flex w-full items-center justify-center bg-blackPrimary">
+    <div
+      className={`transition-all duration-300 ${baseHeaderStyle} ${headerScrollValid}`}
+    >
       <div className="container flex h-[80px] items-center justify-center">
         <Link to="/list">
           <img src={imgLogo} alt="로고" className="w-[200px] cursor-pointer" />
