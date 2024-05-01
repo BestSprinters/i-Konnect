7385a536c916dd54514a69874d45e1b34e208112
김보미 <kimbomi2172@naver.com>
2024-05-01 17:00:20 +0900

    🔥 remove: types 파일 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 import Button from './components/buttons/Button';
 import './index.css';
-import types from './types';
 
 function App() {
   const [isDisabled, setIsDisabled] = useState(false);
@@ -13,26 +12,26 @@ function App() {
 
   return (
     <>
-      <Button type={types.smallSquare}>smallSquare</Button>
-      <Button type={types.smallSquare}>등록</Button>
+      <Button type="smallSquare">smallSquare</Button>
+      <Button type="smallSquare">등록</Button>
 
-      <Button type={types.mediumSquare}>mediumSquare</Button>
-      <Button type={types.mediumSquare}>차트 투표하기</Button>
+      <Button type="mediumSquare">mediumSquare</Button>
+      <Button type="mediumSquare">차트 투표하기</Button>
 
-      <Button type={types.largeSquare} onClick={onClickBtn}>
+      <Button type="largeSquare" onClick={onClickBtn}>
         largeSquare
       </Button>
-      <Button type={types.largeSquare} isDisabled={!isDisabled}>
+      <Button type="largeSquare" isDisabled={!isDisabled}>
         largeSquare
       </Button>
 
-      <Button type={types.round}>round</Button>
+      <Button type="round">round</Button>
 
-      <Button type={types.cancel}>cancel</Button>
-      <Button type={types.cancel}>취소</Button>
+      <Button type="cancel">cancel</Button>
+      <Button type="cancel">취소</Button>
 
-      <Button type={types.more}>more</Button>
-      <Button type={types.more}>더보기</Button>
+      <Button type="more">more</Button>
+      <Button type="more">더보기</Button>
     </>
   );
 }
