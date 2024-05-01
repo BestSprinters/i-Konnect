654a10d98122dca3198117f9497f64a5a48e15c5
Darkthos <kiki7951@naver.com>
2024-05-01 22:35:16 +0900

    ✨ feat: chart.jsx 구조만형성

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import chartIcon from '../assets/imgs/ic_chart.svg';
+import Button from './buttons/Button';
+import ChoiceGender from './charts/ChoiceGender';
+
 function Chart() {
-  return <div>a</div>;
+  return (
+    <div>
+      <div className="flex">
+        <h3 className="1 grow text-base font-bold text-whitePrimary md:text-xl xl:text-2xl">
+          이달의 차트
+        </h3>
+        <Button>
+          <div className="flex items-center justify-center gap-1">
+            <img alt="chartIcon" src={chartIcon} />
+            <p> 차트 투표하기</p>
+          </div>
+        </Button>
+      </div>
+      <div className="flex">
+        <ChoiceGender />
+      </div>
+    </div>
+  );
 }
 
 export default Chart;
---
+++ src/components/charts/ChoiceGender.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ChoiceGender() {
+  return (
+    <>
+      <div className="flex w-full items-center bg-grayDark py-2 text-sm font-normal text-whitePrimary">
+        <p className="mx-auto">이달의 여자 아이돌</p>
+      </div>
+      <div className="flex w-full items-center py-2 text-sm font-normal text-whitePrimary">
+        <p className="mx-auto">이달의 남자 아이돌</p>
+      </div>
+    </>
+  );
+}
+
+export default ChoiceGender;
