fd57e51e4fe5d4648f799c549949353a14c675c5
Darkthos <kiki7951@naver.com>
2024-05-01 15:37:39 +0900

    ✅ test: App.jsx에 getIdols 이미지 렌더링 실험

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import getProducts from './apis/idolsApi';
+import { useEffect, useState } from 'react';
+
+import getCharts from './apis/charts/getChartApi';
+import getDonations from './apis/donations/getDonationsApi';
+import getIdols from './apis/idols/getIdolsApi';
 import './index.css';
 
 function App() {
-  getProducts();
+  const [idolsData, setIdolsData] = useState([]);
+
+  const loadIdolsData = async () => {
+    const result = await getIdols();
+    return setIdolsData(result);
+  };
+
+  useEffect(() => {
+    loadIdolsData();
+    getDonations();
+    getCharts();
+  }, []);
+
   return (
     <div className="flex items-center justify-center">
       <h1>Hello World!</h1>
+      {idolsData?.map((idol) => (
+        <img
+          className="flex h-14 w-14"
+          alt="idols"
+          src={idol.profilePicture}
+          key={idol.id}
+        />
+      ))}
     </div>
   );
 }
