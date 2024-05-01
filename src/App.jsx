d78cd789f4935c995cd493eb4b0874dae5adb69b
CitrusSoda <dndhakus1004@naver.com>
2024-05-01 18:48:49 +0900

    💄  ui: 메인 랜딩페이지 수정 및 전체 틀 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import MainLogo from './assets/imgs/mainlogo.png';
+import LandingPreviewImage from './assets/imgs/landingPreviewImage1.svg';
+import MainLogo from './assets/imgs/mainLogo.png';
 import NewJeansBackground from './assets/imgs/newjeans.svg';
 import LandingSection from './components/landing/LandingSection';
 
@@ -6,11 +7,12 @@ function App() {
   return (
     <main className="relative">
       <div className="absolute -left-[100px] -top-[135px] h-[270px] w-[200px] bg-gradient-radial from-cornerBlue to-blackPrimary opacity-20 blur-2xl" />
-      <div className="container mx-auto">
-        <section className="relative flex h-dvh flex-col items-center text-2xl font-bold text-white">
-          <div className="mt-[100px] md:mt-[120px] lg:mt-[140px]">
-            <h1>내가 좋아하는 아이돌을</h1>
+      <div className="text-2xl font-bold text-white">
+        <section className="relative flex h-dvh flex-col items-center">
+          <div className="mt-[100px] text-center md:mt-[120px] lg:mt-[140px]">
             <h1>
+              내가 좋아하는 아이돌을
+              <br />
               가장 <span className="text-pointOrange">쉽게 덕질</span> 하는 방법
             </h1>
           </div>
@@ -22,12 +24,48 @@ function App() {
           <img
             src={NewJeansBackground}
             alt="new jeans"
-            className="absolute -z-50 h-full max-w-full rounded-full object-cover opacity-70"
+            className="absolute top-1/2 -z-50 max-w-full -translate-y-1/2 rounded-full object-cover opacity-70"
           />
         </section>
-        <LandingSection />
-        <LandingSection />
-        <LandingSection />
+        <LandingSection
+          backgroundImageAlt="뉴진스"
+          backgroundImage={NewJeansBackground}
+          previewImageAlt="first preview image"
+          previewImage={LandingPreviewImage}
+          title="후원하기"
+        >
+          <h1>
+            좋아하는 아이돌에게
+            <br />
+            쉽게 조공해 보세요
+          </h1>
+        </LandingSection>
+        <LandingSection
+          backgroundImageAlt="뉴진스"
+          backgroundImage={NewJeansBackground}
+          previewImageAlt="second preview image"
+          previewImage={LandingPreviewImage}
+          title="이달의 아티스트"
+        >
+          <h1>
+            내 아티스트에게 1등의
+            <br />
+            영예를 선물하세요
+          </h1>
+        </LandingSection>
+        <LandingSection
+          backgroundImageAlt="뉴진스"
+          backgroundImage={NewJeansBackground}
+          previewImageAlt="third preview image"
+          previewImage={LandingPreviewImage}
+          title="나만의 아티스트"
+        >
+          <h1>
+            좋아하는 아티스트의
+            <br />
+            소식을 모아보세요
+          </h1>
+        </LandingSection>
       </div>
     </main>
   );
--- src/components/landing/LandingSection.jsx
+++ src/components/landing/LandingSection.jsx
@@ -1,8 +1,24 @@
-function LandingSection() {
+function LandingSection({
+  children,
+  backgroundImageAlt,
+  backgroundImage,
+  previewImageAlt,
+  previewImage,
+  title,
+}) {
   return (
-    <div className="h-dvh">
-      <h1>Hi</h1>
-    </div>
+    <section className="relative flex h-dvh flex-col items-center">
+      <div className="mt-[10%] text-center">
+        <p className="text-base text-yellowPrimary">{title}</p>
+        {children}
+      </div>
+      <img src={previewImage} alt={previewImageAlt} className="mt-[5%] h-1/2" />
+      <img
+        src={backgroundImage}
+        alt={backgroundImageAlt}
+        className="absolute -z-50 size-full object-cover opacity-70"
+      />
+    </section>
   );
 }
 
