a88d5bcec332ad8dc7609aff9fab1b4f1e6dbcde
Darkthos <kiki7951@naver.com>
2024-05-01 16:42:42 +0900

    ♻️ refactor: getIdolsApi, getDonationsApi 리스트까지 받아오는거에서 데이터까지만 받아오게 변경

--- src/apis/donations/getDonationsApi.js
+++ src/apis/donations/getDonationsApi.js
@@ -3,7 +3,7 @@ import axiosInstance from '../axiosInstance';
 const getDonations = async () => {
   try {
     const response = await axiosInstance.get(`/donations`);
-    const donations = response.data.list;
+    const donations = response.data;
     return donations;
   } catch (error) {
     throw new Error(error.message);
--- src/apis/idols/getIdolsApi.js
+++ src/apis/idols/getIdolsApi.js
@@ -9,7 +9,7 @@ const getIdols = async (cursor = null) => {
 
   try {
     const response = await axiosInstance.get(`/idols?${idolsParams}`);
-    const idols = response.data.list;
+    const idols = response.data;
     return idols;
   } catch (error) {
     throw new Error(error.message);
