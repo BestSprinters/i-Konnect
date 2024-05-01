0674182ca18f83403da87aa47e72b6218dc77c8e
Darkthos <kiki7951@naver.com>
2024-05-01 16:39:41 +0900

    📝 docs: Chart 컴포넌트 생성 App.jsx 시작 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
 // ! TEST : 테스트용 이미지입니다
-import { useState } from 'react';
-
-import Minji from './assets/images/test.png';
-import IdolAvatar from './components/IdolAvatar';
-import Button from './components/buttons/Button';
+import Chart from './components/Chart';
 import './index.css';
 
 function App() {
-  const [isDisabled, setIsDisabled] = useState(false);
-  const types = {
-    largeSquare: 'largeSquare',
-    smallSquare: 'smallSquare',
-    round: 'round',
-  };
-
-  const onClickBtn = () => {
-    setIsDisabled(isDisabled);
-  };
-
   return (
-    <>
-      <Button onClick={onClickBtn} type={types.largeSquare}>
-        버튼활성화
-      </Button>
-      <Button onClick={onClickBtn} isDisabled={!isDisabled}>
-        버튼비활성화
-      </Button>
-      <Button type={types.smallSquare}>차트 투표하기</Button>
-      <Button type={types.round}>추가하기</Button>
-
-      <div>
-        <IdolAvatar alt="민지" src={Minji} size="small" />
-        <IdolAvatar alt="항상" src={Minji} size="medium" />
-        <IdolAvatar alt="화이팅" src={Minji} size="large" />
-      </div>
-    </>
+    <div>
+      <Chart />
+    </div>
   );
 }
 
---
+++ src/components/Chart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Chart() {
+  return <div>a</div>;
+}
+
+export default Chart;
