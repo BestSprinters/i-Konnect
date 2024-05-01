3c2fb098b1b85ca15d085f08557acb130d9b5b2d
Darkthos <kiki7951@naver.com>
2024-05-01 22:40:37 +0900

    🐛 fix: map 잘 렌더링되게

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import { useEffect, useState } from 'react';
 
-import getCharts from './apis/charts/getChartApi';
-import getDonations from './apis/donations/getDonationsApi';
 import getIdols from './apis/idols/getIdolsApi';
 import './index.css';
 
 function App() {
   const [idolsData, setIdolsData] = useState([]);
 
-  const loadIdolsData = async () => {
-    const result = await getIdols();
-    return setIdolsData(result);
-  };
-
   useEffect(() => {
+    const loadIdolsData = async () => {
+      const result = await getIdols();
+      return setIdolsData(result);
+    };
     loadIdolsData();
-    getDonations();
-    getCharts();
   }, []);
 
   return (
     <div className="flex items-center justify-center">
       <h1>Hello World!</h1>
-      {idolsData?.map((idol) => (
+      {idolsData.list?.map((idol) => (
         <img
           className="flex h-14 w-14"
           alt="idols"
