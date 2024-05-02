6984778d7136b2c621e001ca557e2273f96feef5
김보미 <kimbomi2172@naver.com>
2024-05-02 16:31:08 +0900

    💄 ui: 크레딧 input range ui 구현

--- src/pages/AddSponsor.jsx
+++ src/pages/AddSponsor.jsx
@@ -1,9 +1,35 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '../components/buttons/Button';
 
 function AddSponsor() {
+  const [value, setValue] = useState(0);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(1000);
+
+  const onChangeInput = () => {
+    const inputElements = document.querySelectorAll('.inputRange');
+
+    inputElements.forEach((element) => {
+      const input = element.getElementsByTagName('input')[0];
+      const output = element.getElementsByTagName('output')[0];
+
+      if (!input || !output) return;
+
+      const val = parseFloat(input.value);
+      const minVal = parseFloat(input.min) || 0;
+      const maxVal = parseFloat(input.max) || 1000;
+      setMin(minVal);
+      setMax(maxVal);
+      setValue(val);
+    });
+
+    // output.textContent = val;
+    // const inputStyle = 'bg-gradient-to-r from-pointOrange to-pointPink';
+  };
+  const inputStyle = 'bg-gradient-to-r from-pointOrange to-pointPink';
+
   return (
     <div className="container mx-auto mt-[80px] flex w-full max-w-[1200px] items-center justify-center">
       <form className="mx-8 w-full">
@@ -84,23 +110,25 @@ function AddSponsor() {
         </div>
 
         <div className="col-span-full my-8">
-          <label
-            htmlFor="content"
-            className="block text-sm font-medium leading-6 text-whitePrimary"
-          >
-            지하철 역
-          </label>
-          <div className="mt-2">ha..</div>
-        </div>
-
-        <div className="col-span-full my-8">
-          <label
-            htmlFor="content"
-            className="block text-sm font-medium leading-6 text-whitePrimary"
-          >
+          <label className="block text-sm font-medium leading-6 text-whitePrimary">
             필요한 크레딧
           </label>
-          <div className="mt-2">선 해가지구 설정하기</div>
+          <div className="flex w-full items-center justify-center">
+            <input
+              onChange={onChangeInput}
+              type="range"
+              name="credit"
+              id="credit"
+              value="0"
+              min={min}
+              step="1"
+              max={max}
+              className={`${inputStyle} m-0 h-1 w-full appearance-none rounded-full p-0 shadow-none outline-none [&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:w-5 [&::-webkit-slider-thumb]:cursor-grab [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-pointOrange`}
+            />
+            <output className="pointer-events-none relative ml-1 w-6 text-center text-sm font-medium text-white">
+              {value}
+            </output>
+          </div>
         </div>
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
