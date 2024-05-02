d8d7e9d11b47216b79e3e375e2188bd7600eb71c
kimhyesun <hyesun1004.kim@gmail.com>
2024-05-02 18:00:52 +0900

    ✨ feat: CheckedIdolAvatar 생성

---
+++ src/components/CheckedIdolAvatar.jsx
@@ -0,0 +1,30 @@
+import IcCheck from '../assets/imgs/ic_check.svg';
+
+function IdolAvatar({ alt, src, size }) {
+  const sizes = {
+    small: 'size-[70px]',
+    medium: 'size-[100px]',
+    large: 'size-[128px]',
+  };
+
+  return (
+    <div
+      className={`rounded-full border-2 ${sizes[size]} relative border-pointOrangePink p-[6px]`}
+    >
+      <img
+        src={IcCheck}
+        alt="체크 표시"
+        className="absolute inset-0 z-20 m-auto size-[52px]"
+      />
+      <div className="z-10 size-full overflow-hidden rounded-full  bg-gradient-to-r from-[#F96E68] to-[#FE578F] object-cover">
+        <img
+          src={src}
+          alt={alt}
+          className="size-full object-cover opacity-50"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default IdolAvatar;
---
+++ src/components/FavoriteIdol.jsx
@@ -0,0 +1,4 @@
+function FavoriteIdol() {
+  return <div>favoriteIdol</div>;
+}
+export default FavoriteIdol;
