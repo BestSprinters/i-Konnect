bfeeca39fc91a7f6790541244fe32509a6f2010f
heony704 <heony704@gamil.com>
2024-05-03 05:25:14 +0900

    ♻️ refactor: 모달을 상위에서 렌더링
    - 모달을 사용하는 컴포넌트가 아니라 상위 태그에서 렌더링
    - ReactDOM.createPortal 사용

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 import closeIcon from '../assets/imgs/ic_close.svg';
 
@@ -16,30 +17,35 @@ function Modal({ title, type, open, onClose, children }) {
   const modalStyle = type === 'wide' ? 'w-[524px]' : 'w-[344px]';
 
   return (
-    <div
-      className="fixed inset-0 flex h-full w-full items-center justify-center bg-blackPrimary/80"
-      ref={modalBackdropRef}
-      onClick={(e) => handleModalBackdropClick(e)}
-      aria-hidden="true"
-    >
-      <div
-        className={`${modalStyle} relative rounded-xl bg-blackSecondary p-5`}
-      >
-        <button
-          type="button"
-          className="absolute right-4 top-4 rounded-xl p-1 hover:bg-grayBlue/20 focus:bg-grayBlue/50"
-          onClick={onClose}
+    <>
+      {createPortal(
+        <div
+          className="fixed inset-0 flex h-full w-full items-center justify-center bg-blackPrimary/80"
+          ref={modalBackdropRef}
+          onClick={(e) => handleModalBackdropClick(e)}
+          aria-hidden="true"
         >
-          <img className="h-6 w-6" src={closeIcon} alt="닫기 아이콘" />
-        </button>
-        {title && (
-          <h2 className="mb-5 text-lg font-medium leading-6 text-whiteSecondary">
-            {title}
-          </h2>
-        )}
-        <div>{children}</div>
-      </div>
-    </div>
+          <div
+            className={`${modalStyle} relative rounded-xl bg-blackSecondary p-5`}
+          >
+            <button
+              type="button"
+              className="absolute right-4 top-4 rounded-xl p-1 hover:bg-grayBlue/20 focus:bg-grayBlue/50"
+              onClick={onClose}
+            >
+              <img className="h-6 w-6" src={closeIcon} alt="닫기 아이콘" />
+            </button>
+            {title && (
+              <h2 className="mb-5 text-lg font-medium leading-6 text-whiteSecondary">
+                {title}
+              </h2>
+            )}
+            <div>{children}</div>
+          </div>
+        </div>,
+        document.getElementById('modal-root'),
+      )}
+    </>
   );
 }
 
