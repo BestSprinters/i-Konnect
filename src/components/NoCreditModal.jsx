2134f72e1784cd9e81a7ac4424e97a45877a760c
heony704 <heony704@gamil.com>
2024-05-03 21:21:15 +0900

    ✨ feat: 크레딧 부족 모달창 컴포넌트 구현

---
+++ src/components/NoCreditModal.jsx
@@ -0,0 +1,20 @@
+import creditImage from '../assets/imgs/img_credit.svg';
+import Modal from './Modal';
+import Button from './buttons/Button';
+
+function NoCreditModal({ open, onClose }) {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <img src={creditImage} alt="크레딧" />
+      <p className="mb-8 font-medium">
+        앗! 투표하기 위한 <span className="text-pointOrange">크레딧</span>이
+        부족해요
+      </p>
+      <Button type="largeSquare" onClick={onClose}>
+        확인
+      </Button>
+    </Modal>
+  );
+}
+
+export default NoCreditModal;
