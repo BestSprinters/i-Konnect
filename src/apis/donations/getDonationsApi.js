8c5649bec3e57ccf8f28f0a3c8457af0d9dd4e45
Darkthos <kiki7951@naver.com>
2024-05-04 16:25:40 +0900

    📦 chore: 주석추가

--- src/apis/donations/getDonationsApi.js
+++ src/apis/donations/getDonationsApi.js
@@ -1,7 +1,8 @@
 import createQueryParams from '../../utils/createQueryParams';
 import axiosInstance from '../axiosInstance';
 
-// https://fandom-k-api.vercel.app/6-15/donations?cursor=142&pageSize=10&priorityIdolIds=21&priorityIdolIds=26&priorityIdolIds=27&priorityIdolIds=28&priorityIdolIds=29'
+// priorityIdolIds 옵션 쓰실때 2개 이상이면,
+// priorityIdolIds: [21, 26, 27] <= 이런식으로 불러올 컴포넌트에서 **배열**로 만들어 주셔야 합니다.(하나만 불러올시에는 pageSize처럼 쓰시면 됩니다.)
 const getDonations = async (option = {}) => {
   const defaultOption = {
     cursor: '',
