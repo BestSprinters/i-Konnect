750a912d494f96909e813efc4452ace93492eb6a
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-05-08 20:56:47 +0900

    ✨ feat: 조공 등록 폼 구현 (#31)
    * ✨ feat: 기본 버튼 생성
    
    * ✨ feat: type에 따른 버튼 스타일링
    
    * 🐛 fix: eslint 주석 제거 후 버튼 컴포넌트 코드 수정
    
    * ♻️ refacotr: eslint 적용 후, 코드 재수정
    
    * ✨ feat: 기본 폼 UI 구현
    
    * ♻️ refactor: 버튼 type name 변경
    
    - 특성에서 스타일네임으로 변경
    - 버튼 유효성 이름 style추가
    
    * ♻️ refactor: types 파일 분리
    
    * ✨ feat: type에 따른 버튼 스타일링 수정
    
    * ✨ feat: 버튼 스타일 적용
    
    * 🔥 remove: types 파일 코드 제거
    
    * 🔥 remove: mediumSquare 삭제
    
    * ♻️ refactor: 기본 버튼 스타일 변수 삭제
    
    * ♻️ refactor: 버튼 타입에 따른 클래스 스타일 수정
    
    * ♻️ refactor: 버튼 비활성화 유무
    
    * 💄 ui: 버튼 radius 수정
    
    * ✨ feat: 기본 폼 구현
    
    * 💄 ui: 크레딧 input range ui 구현
    
    * ✨ feat: input range 바 생성
    
    * 📦 chore: max credit 수정
    
    * ✨ feat: 아이돌 리스트 띄우기
    
    * ♻️ refactor: 프로그레스 바 단위 수정
    
    * 🔀 Merge branch 'main' of https://github.com/Darkthos/i-Konnect into feat/addSponsor
    
    * 📦 chore: swiper 설치
    
    * 🔥 remove: 불필요한 주석 코드 제거
    
    * ✨ feat: 아이돌 리스트 스와이퍼 구현
    
    * ♻️ refactor: 크레딧 range step 100으로 수정
    
    * ✨ feat: 폼 데이터 값 확인
    
    * ✨ feat: 폼 데이터 출력
    
    * 💄 ui: 지하철 역 선택 ui 초기 구현
    
    * ♻️ refactor: 데이터 폼 입력 값 출력
    
    * 🐛 fix: cors 이슈 해결
    
    * ✨ feat: 폼 데이터 POST 요청
    
    * 🐛 fix: 키 이슈 해결
    
    * ✨ feat: 폼 입력값 유효성 추가
    
    * 💄 ui: 아이돌 검색 UI 구현
    
    * 💄 ui: 버튼 컴포넌트 스타일 수정
    
    * ✨ feat: 아이돌 검색 기능 구현
    
    * ✨ feat: 아이돌 선택시 체크 표시
    
    * ✨ feat: 폼 등록 후 list 페이지로 이동

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -4,6 +4,12 @@ const teamName = '6-15';
 
 const axiosConfig = {
   baseURL: `https://fandom-k-api.vercel.app/${teamName}`,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  },
 };
 
 const axiosInstance = axios.create(axiosConfig);
