b682da3b635c62ca0e13cf66e8b9c2ffff51efc5
김보미 <kimbomi2172@naver.com>
2024-05-10 18:17:59 +0900

    🐛 fix: 오타 수정

--- src/utils/createButtonStyle.js
+++ src/utils/createButtonStyle.js
@@ -6,7 +6,7 @@ const createButtonStyle = (isDisabled) => {
     largeSquareBlack: `w-[326px] h-[42px] rounded-[3px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumRoundPrimary: `max-w-[256px] w-full h-[48px] rounded-3xl ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSquarePrimary: `w-[180px] h-[42px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSqaurePrimary: `w-[180px] h-[42px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
   };
   return buttonTypeStyle;
 };
