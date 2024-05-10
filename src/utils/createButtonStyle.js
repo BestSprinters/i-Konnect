826d115f4bd4d9ef70620266c8fd5b645a29f59e
김보미 <kimbomi2172@naver.com>
2024-05-10 19:49:59 +0900

    ♻️ refactor: fullSquare max-w 제거

--- src/utils/createButtonStyle.js
+++ src/utils/createButtonStyle.js
@@ -1,7 +1,7 @@
 const createButtonStyle = (isDisabled) => {
   const buttonTypeStyle = {
     xlargeSquarePrimary: `max-w-[476px] w-full h-[48px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
-    fullSquarePrimary: `max-w-[476px] w-full h-[42px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
+    fullSquarePrimary: `w-full h-[42px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
     fitSquarePrimary: `h-[32px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
     largeSquareBlack: `w-[326px] h-[42px] rounded-[3px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumRoundPrimary: `max-w-[256px] w-full h-[48px] rounded-3xl ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
