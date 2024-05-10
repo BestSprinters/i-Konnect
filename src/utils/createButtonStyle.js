8a8485d6261e17a228f47ebed56c9e9e4865a3cb
김보미 <kimbomi2172@naver.com>
2024-05-10 17:48:22 +0900

    🐛 fix: 베경 색 오타 수정

--- src/utils/createButtonStyle.js
+++ src/utils/createButtonStyle.js
@@ -5,7 +5,7 @@ const createButtonStyle = (isDisabled) => {
     fitSquarePrimary: `h-[32px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
     largeSquareBlack: `w-[326px] h-[42px] rounded-[3px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumRoundPrimary: `max-w-[256px] w-full h-[48px] rounded-3xl ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecendary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumSquarePrimary: `w-[180px] h-[42px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
   };
   return buttonTypeStyle;
