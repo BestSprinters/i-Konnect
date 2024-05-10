3d3fc59590b5ec8145969c0e5d29b9f93aa93cf7
kimhyesun <hyesun1004.kim@gmail.com>
2024-05-11 05:55:35 +0900

    💄 ui: 아이돌리스크 버튼 위치 및 제목 위치 변경

--- src/components/MyPageIdol.jsx
+++ src/components/MyPageIdol.jsx
@@ -41,74 +41,75 @@ function MyPageIdol({ idols, onChange }) {
   const mobileSize = useMediaQuery('(max-width: 767px)');
 
   return (
-    <div className="relative mt-[42px] border-t border-[#FFFFFF1A] px-[50px] pt-[42px] mobile:mx-[24px] mobile:px-[0] tablet:px-[70px]">
+    <div className="relative mt-[42px] border-t border-[#FFFFFF1A] pt-[42px] mobile:mx-[24px] mobile:px-[0] tablet:px-[70px]">
       <h2 className="mb-[32px] text-2xl font-semibold">
         관심있는 아이돌을 추가해보세요.
       </h2>
-      <Swiper
-        slidesPerView={3}
-        grid={{
-          rows: 2,
-        }}
-        spaceBetween={12}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        breakpoints={{
-          767: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-            grid: {
-              rows: 2,
+      <div className="desktop:px-[50px] ">
+        <Swiper
+          slidesPerView={3}
+          grid={{
+            rows: 2,
+          }}
+          spaceBetween={12}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          breakpoints={{
+            767: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+              grid: {
+                rows: 2,
+              },
             },
-          },
-          1024: {
-            slidesPerView: 6,
-            spaceBetween: 20,
-            grid: {
-              rows: 2,
+            1024: {
+              slidesPerView: 6,
+              spaceBetween: 20,
+              grid: {
+                rows: 2,
+              },
             },
-          },
-          1280: {
-            slidesPerView: 8,
-            spaceBetween: 20,
-            grid: {
-              rows: 2,
+            1280: {
+              slidesPerView: 8,
+              spaceBetween: 20,
+              grid: {
+                rows: 2,
+              },
             },
-          },
-        }}
-        modules={[Grid, Navigation]}
-        className="h-[400px]"
-      >
-        {idols?.map((idol) => (
-          <SwiperSlide key={idol.id}>
-            <button
-              type="button"
-              onClick={() => handleIdolToggle(idol)}
-              aria-label={`${idol.name} 썸네일`}
-            >
-              <IdolThumbnail
-                size={mobileSize ? 'medium' : 'large'}
-                checked={IsFavorite.includes(idol)}
-                name={idol.name}
-                group={idol.group}
-                src={idol.profilePicture}
-              />
-            </button>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
+          }}
+          modules={[Grid, Navigation]}
+          className="h-[400px]"
+        >
+          {idols?.map((idol) => (
+            <SwiperSlide key={idol.id}>
+              <button
+                type="button"
+                onClick={() => handleIdolToggle(idol)}
+                aria-label={`${idol.name} 썸네일`}
+              >
+                <IdolThumbnail
+                  size={mobileSize ? 'medium' : 'large'}
+                  checked={IsFavorite.includes(idol)}
+                  name={idol.name}
+                  group={idol.group}
+                  src={idol.profilePicture}
+                />
+              </button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
       <div
         ref={prevRef}
-        className="absolute left-0 top-[50%] z-[50] flex h-[135px] w-[30px] items-center bg-[#1B1B1BCC] mobile:hidden tablet:left-[20px]"
+        className="absolute left-0 top-[50%] z-[50]  hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:left-[20px] tablet:flex desktop:flex"
       >
         <img src={prevArrow} alt="prevArrow" />
       </div>
       <div
         ref={nextRef}
-        className="absolute right-0 top-[50%] z-[6] flex h-[135px] w-[30px] items-center bg-[#1B1B1BCC] mobile:hidden tablet:right-[20px]"
+        className="absolute right-0 top-[50%] z-[6]  hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:right-[20px] tablet:flex desktop:flex"
       >
         <img src={nextArrow} alt="nextArrow" />
       </div>
