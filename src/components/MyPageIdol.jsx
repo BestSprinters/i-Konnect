0e900c2ad5fe32f79ce9e821580a269301f31cde
kimhyesun <hyesun1004.kim@gmail.com>
2024-05-11 21:03:16 +0900

    💄 ui: 캐러셀 버튼 위치 조정

--- src/components/MyPageIdol.jsx
+++ src/components/MyPageIdol.jsx
@@ -50,89 +50,90 @@ function MyPageIdol({ idols, onChange }) {
   const mobileSize = useMediaQuery('(max-width: 767px)');
 
   return (
-    <div className="relative mt-[42px] border-t border-[#FFFFFF1A] pt-[42px] mobile:mx-[24px] mobile:px-[0] tablet:px-[70px]">
+    <div className="mt-[42px] border-t border-[#FFFFFF1A] pt-[42px] mobile:mx-[24px] mobile:px-[0] tablet:px-[70px]">
       <h2 className="mb-[32px] text-2xl font-semibold">
         관심있는 아이돌을 추가해보세요.
       </h2>
+      <div className="relative">
+        <div className="desktop:px-[50px] ">
+          <Swiper
+            slidesPerView={3}
+            grid={{
+              rows: 2,
+            }}
+            spaceBetween={12}
+            navigation={{
+              prevEl: prevRef.current,
 
-      <div className="desktop:px-[50px] ">
-        <Swiper
-          slidesPerView={3}
-          grid={{
-            rows: 2,
-          }}
-          spaceBetween={12}
-          navigation={{
-            prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            breakpoints={{
+              767: {
+                slidesPerView: 4,
 
-            nextEl: nextRef.current,
-          }}
-          breakpoints={{
-            767: {
-              slidesPerView: 4,
+                spaceBetween: 20,
 
-              spaceBetween: 20,
-
-              grid: {
-                rows: 2,
+                grid: {
+                  rows: 2,
+                },
               },
-            },
 
-            1024: {
-              slidesPerView: 6,
+              1024: {
+                slidesPerView: 6,
 
-              spaceBetween: 20,
+                spaceBetween: 20,
 
-              grid: {
-                rows: 2,
+                grid: {
+                  rows: 2,
+                },
               },
-            },
 
-            1280: {
-              slidesPerView: 8,
+              1280: {
+                slidesPerView: 8,
 
-              spaceBetween: 20,
+                spaceBetween: 20,
 
-              grid: {
-                rows: 2,
+                grid: {
+                  rows: 2,
+                },
               },
-            },
-          }}
-          modules={[Grid, Navigation]}
-          className="h-[400px]"
+            }}
+            modules={[Grid, Navigation]}
+            className="h-[400px]"
+          >
+            {idols?.map((idol) => (
+              <SwiperSlide key={idol.id}>
+                <button
+                  type="button"
+                  onClick={() => handleIdolToggle(idol)}
+                  aria-label={`${idol.name} 썸네일`}
+                >
+                  <IdolThumbnail
+                    size={mobileSize ? 'medium' : 'large'}
+                    checked={IsFavorite.includes(idol)}
+                    name={idol.name}
+                    group={idol.group}
+                    src={idol.profilePicture}
+                  />
+                </button>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div
+          ref={prevRef}
+          className="absolute left-0 top-[50%] z-[50] hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:left-[20px] tablet:flex desktop:flex"
         >
-          {idols?.map((idol) => (
-            <SwiperSlide key={idol.id}>
-              <button
-                type="button"
-                onClick={() => handleIdolToggle(idol)}
-                aria-label={`${idol.name} 썸네일`}
-              >
-                <IdolThumbnail
-                  size={mobileSize ? 'medium' : 'large'}
-                  checked={IsFavorite.includes(idol)}
-                  name={idol.name}
-                  group={idol.group}
-                  src={idol.profilePicture}
-                />
-              </button>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+          <img src={prevArrow} alt="prevArrow" />
+        </div>
 
-      <div
-        ref={prevRef}
-        className="absolute left-0 top-[50%] z-[50] hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:left-[20px] tablet:flex desktop:flex"
-      >
-        <img src={prevArrow} alt="prevArrow" />
-      </div>
-
-      <div
-        ref={nextRef}
-        className="absolute right-0 top-[50%] z-[6] hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:right-[20px] tablet:flex desktop:flex"
-      >
-        <img src={nextArrow} alt="nextArrow" />
+        <div
+          ref={nextRef}
+          className="absolute right-0 top-[50%] z-[6] hidden h-[135px] w-[30px] items-center justify-center bg-[#1B1B1BCC] tablet:right-[20px] tablet:flex desktop:flex"
+        >
+          <img src={nextArrow} alt="nextArrow" />
+        </div>
       </div>
 
       <div className="mt-[32px] flex justify-center">
