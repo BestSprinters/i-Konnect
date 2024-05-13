23e0dfee5dc140ab0a82d8d359b2da1cfbb35bef
김보미 <kimbomi2172@naver.com>
2024-05-13 12:44:56 +0900

    💄 ui: medium 버튼 rounded 추가

--- src/utils/createButtonStyle.js
+++ src/utils/createButtonStyle.js
@@ -5,8 +5,8 @@ const createButtonStyle = (isDisabled) => {
     fitSquarePrimary: `h-[32px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
     largeSquareBlack: `w-[326px] h-[42px] rounded-[3px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumRoundPrimary: `max-w-[256px] w-full h-[48px] rounded-3xl ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSqaurePrimary: `w-[180px] h-[42px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border rounded-[3px] ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSqaurePrimary: `w-[180px] h-[42px] rounded-[3px] ${!isDisabled ? 'bg-gradient-to-r from-pointOrange to-pointPink' : 'cursor-not-allowed bg-grayMedium'}`,
   };
   return buttonTypeStyle;
 };
