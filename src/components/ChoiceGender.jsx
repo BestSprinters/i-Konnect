905d372eff042b9c13c8b907221b04961748af10
Darkthos <kiki7951@naver.com>
2024-05-14 21:11:16 +0900

    💄 ui: 아이돌 성별 선택 버튼 css 개선

--- src/components/ChoiceGender.jsx
+++ src/components/ChoiceGender.jsx
@@ -12,21 +12,27 @@ function ChoiceGender({ handleClickChangeGender }) {
     <>
       <button
         type="button"
-        className={`flex w-full items-center ${gender === 'female' ? 'border-b bg-white/10' : ''} py-2 text-sm font-normal text-whitePrimary`}
+        className={`flex w-full items-center bg-blackSecondary ${gender === 'female' ? 'border-b bg-white/20' : ''} py-2 text-sm font-normal text-whitePrimary`}
         onClick={() => handleClickChangeButtonStyle('female')}
         tabIndex="0"
         aria-label="Change to female"
       >
-        <p className="mx-auto">이달의 여자 아이돌</p>
+        <p className={`mx-auto ${gender === 'male' ? 'text-grayMedium' : ''}`}>
+          이달의 여자 아이돌
+        </p>
       </button>
       <button
         type="button"
-        className={`flex w-full items-center ${gender === 'male' ? 'border-b bg-white/10' : ''} py-2 text-sm font-normal text-whitePrimary`}
+        className={`flex w-full items-center bg-blackSecondary ${gender === 'male' ? 'border-b bg-white/20' : ''} py-2 text-sm font-normal text-whitePrimary`}
         onClick={() => handleClickChangeButtonStyle('male')}
         tabIndex="0"
         aria-label="Change to male"
       >
-        <p className="mx-auto">이달의 남자 아이돌</p>
+        <p
+          className={`mx-auto ${gender === 'female' ? 'text-grayMedium' : ''}`}
+        >
+          이달의 남자 아이돌
+        </p>
       </button>
     </>
   );
