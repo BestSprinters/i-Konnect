455cf8c32a32735035da3d18b19753fda33212ff
Seungheon Lee <36994104+heony704@users.noreply.github.com>
2024-05-14 21:19:16 +0900

    ✨ feat: 조공 리스트에서 내가 관심있는 아이돌만 볼 수 있는 기능 (#62)
    * ✨ feat: 조공 리스트에서 내가 관심있는 아이돌만 볼 수 있는 기능
    
    * 💄 ui: 내가 관심있는 아이돌 버튼 UI 변경
    
    * 🐛 fix: 내가 관심있는 아이돌이 존재하지 않을 때 오류 수정
    
    * 💄 ui: 내가 관심있는 아이돌 토글 버튼 위치 변경 및 반응형 적용

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -10,6 +10,7 @@ import MyCredit from '../components/MyCredit';
 import SponsorPagination from '../components/SponsorPagination';
 import SponsorSlider from '../components/SponsorSlider';
 import useMediaQuery from '../hooks/useMediaQuery';
+import useToggle from '../hooks/useToggle';
 
 function ListPage() {
   const tabletSize = useMediaQuery('(max-width: 1280px)');
@@ -22,24 +23,38 @@ function ListPage() {
     pageSize: 10,
   };
 
-  // 초기 데이터 로딩
+  const [isOnlyFavorite, toggleOnlyFavorite] = useToggle(false);
+
+  // 데이터 로딩
   useEffect(() => {
-    const fetchInitialData = async () => {
-      const initialFetchOption = {
+    const fetchData = async () => {
+      const FetchOption = {
         cursor: '',
-        pageSize: 10,
+        pageSize: isOnlyFavorite ? 10000 : 10,
       };
-      try {
-        const result = await getDonations(initialFetchOption);
-        setDonations(result.list);
-        setNextCursor(result.nextCursor);
-      } catch (error) {
-        console.error('데이터를 불러오지 못했습니다.', error);
+
+      const { list: donationList, nextCursor: donationNextCursor } =
+        await getDonations(FetchOption);
+
+      if (isOnlyFavorite) {
+        const favoriteIdols =
+          JSON.parse(localStorage.getItem('MyPage_FavoriteIdol')) ?? [];
+        const favoriteIdolIds = favoriteIdols.map(
+          (favoriteIdol) => favoriteIdol.id,
+        );
+        const filteredDonations = donationList.filter((donation) =>
+          favoriteIdolIds.includes(donation.idol.id),
+        );
+
+        setDonations(filteredDonations);
+      } else {
+        setDonations(donationList);
+        setNextCursor(donationNextCursor);
       }
     };
 
-    fetchInitialData();
-  }, []);
+    fetchData();
+  }, [isOnlyFavorite]);
 
   // 추가 데이터 로딩
   const handleReachEnd = async () => {
@@ -61,14 +76,34 @@ function ListPage() {
       <InnerAnimation>
         <MyCredit />
         <div className="mt-10 tablet:mt-16">
-          <div className="mx-6 flex items-end justify-between desktop:m-0">
-            <h1 className="text-bold text-base tablet:text-xl desktop:text-2xl">
-              후원을 기다리는 조공
-            </h1>
-            <LinkButton type="fitSquarePrimary" to="/addSponsor">
-              <img src={icPlus} alt="" className="mr-1" />
-              조공 등록하기
-            </LinkButton>
+          <div className="mx-6 flex flex-col gap-1 tablet:gap-2 desktop:m-0 desktop:gap-3">
+            <div className="flex items-center justify-between">
+              <h1 className="text-bold text-base text-whitePrimary tablet:text-xl desktop:m-0 desktop:text-2xl">
+                후원을 기다리는 조공
+              </h1>
+              <LinkButton type="fitSquarePrimary" to="/addSponsor">
+                <img src={icPlus} alt="" className="mr-1" />
+                조공 등록하기
+              </LinkButton>
+            </div>
+            <div className="flex items-center">
+              <label
+                className="inline-flex cursor-pointer items-center"
+                htmlFor="toggle"
+              >
+                <input
+                  type="checkbox"
+                  value={isOnlyFavorite}
+                  onClick={toggleOnlyFavorite}
+                  className="peer sr-only"
+                  id="toggle"
+                />
+                <div className="peer relative h-5 w-9 rounded-full border-gray-600 bg-gray-700 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-whitePrimary after:bg-whitePrimary after:transition-all after:content-[''] peer-checked:bg-pointOrangePink peer-checked:after:translate-x-full peer-checked:after:border-whitePrimary peer-focus:outline-none peer-focus:ring-[3px] peer-focus:ring-pointOrangePink/40 mobile:h-4 mobile:w-7 after:mobile:h-3 after:mobile:w-3 rtl:peer-checked:after:-translate-x-full" />
+                <span className="ml-2 text-sm font-medium text-grayLight mobile:text-xs">
+                  내가 관심있는 아이돌
+                </span>
+              </label>
+            </div>
           </div>
           {tabletSize ? (
             <SponsorSlider donations={donations} onReachEnd={handleReachEnd} />
