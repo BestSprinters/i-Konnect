24e5a1a091b1e8c338807b567e361916b7b3dec5
heony704 <heony704@gmail.com>
2024-05-14 16:31:19 +0900

    💄 ui: 반응형 디자인 수정

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -59,8 +59,8 @@ function ListPage() {
       <Header />
       <MyCredit />
       <div className="mt-10 tablet:mt-16">
-        <div className="mr-6 flex flex-row justify-between">
-          <h1 className="text-bold mx-6 text-base tablet:text-xl desktop:m-0 desktop:text-2xl">
+        <div className="mx-6 flex items-end justify-between desktop:m-0">
+          <h1 className="text-bold text-base tablet:text-xl desktop:text-2xl">
             후원을 기다리는 조공
           </h1>
           <LinkButton type="fitSquarePrimary" to="/addSponsor">
