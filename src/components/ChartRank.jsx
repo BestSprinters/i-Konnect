a5a9148c36d75811b5b9fe4e4e0d68ed8eb92605
kimminjae <162538553+Darkthos@users.noreply.github.com>
2024-05-15 17:27:37 +0900

    💄 ui: 순위 색 변경, 아이돌 사진과 순위 간격 조정 (#75)
    * 💄 ui: 검색 css 수정
    
    * 💄 ui: 아이돌 성별 선택 버튼 css 개선
    
    * 💄 ui: 성별 버튼, 검색 창 border-radius 차트 투표하기와 같게 맞춤
    
    * 💄 ui: 투표 순위텍스트 색, margin변경
    
    * 💄 ui: 간격 더 예브게 수정

--- src/components/ChartRank.jsx
+++ src/components/ChartRank.jsx
@@ -14,9 +14,9 @@ function ChartRank({
     <div
       className={`h-17 flex w-full items-center py-2 ${hideBorder ? '' : 'border-b border-white/10'}`}
     >
-      <div className="1 flex grow items-center gap-2">
+      <div className="1 flex grow items-center gap-3 text-center">
         <IdolAvatar size="small" src={profilePicture} />
-        <p className="w-4 text-sm font-medium">{rank}</p>
+        <p className="w-4 text-sm font-medium text-pointOrange/60">{rank}</p>
         <p className="text-sm font-medium">
           {group} {name}
         </p>
