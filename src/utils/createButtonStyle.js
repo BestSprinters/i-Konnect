59d32463db5d03db98e8518bde5ea248e439ef97
heony704 <heony704@gmail.com>
2024-05-15 12:48:18 +0900

    🐛 fix: 버튼 타입 스펠링 오류 수정

--- src/utils/createButtonStyle.js
+++ src/utils/createButtonStyle.js
@@ -6,8 +6,8 @@ const createButtonStyle = (isDisabled) => {
     fitSquarePrimary: `h-[32px] rounded-[3px] ${!isDisabled ? 'transition-all duration-500 bg-pos-0 hover:bg-pos-100 bg-size-200 bg-gradient-to-r from-pointOrange via-pointPink to-pointOrange hover:scale-105' : 'cursor-not-allowed bg-grayMedium'}`,
     largeSquareBlack: `w-[326px] h-[42px] rounded-[3px] bg-blackSecondary border-whiteSecondary-500 border hover:bg-blackHover transition duration-500 ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
     mediumRoundPrimary: `max-w-[256px] w-full h-[48px] rounded-3xl ${!isDisabled ? 'transition-all duration-500 bg-pos-0 hover:bg-pos-100 bg-size-200 bg-gradient-to-r from-pointOrange via-pointPink to-pointOrange hover:scale-105' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSqaureBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border rounded-[3px] hover:bg-blackHover transition duration-500 ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
-    mediumSqaurePrimary: `w-[180px] h-[42px] rounded-[3px] ${!isDisabled ? 'transition-all duration-500 bg-pos-0 hover:bg-pos-100 bg-size-200 bg-gradient-to-r from-pointOrange via-pointPink to-pointOrange hover:scale-105' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSquareBlack: `w-[180px] h-[42px] bg-blackSecondary border-whiteSecondary-500 border rounded-[3px] hover:bg-blackHover transition duration-500 ${!isDisabled ? '' : 'cursor-not-allowed bg-grayMedium'}`,
+    mediumSquarePrimary: `w-[180px] h-[42px] rounded-[3px] ${!isDisabled ? 'transition-all duration-500 bg-pos-0 hover:bg-pos-100 bg-size-200 bg-gradient-to-r from-pointOrange via-pointPink to-pointOrange hover:scale-105' : 'cursor-not-allowed bg-grayMedium'}`,
   };
   return buttonTypeStyle;
 };
