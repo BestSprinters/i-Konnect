2685e6bade8b62ea8b70bd9567dad49973522a3f
김보미 <kimbomi2172@naver.com>
2024-05-15 17:37:06 +0900

    💄 ui: 이미지 여백 추가

--- src/components/VoteSuccessModal.jsx
+++ src/components/VoteSuccessModal.jsx
@@ -1,11 +1,11 @@
-import ImgVoteSuccess from '../assets/imgs/img_vote_success.png';
+import imgVoteSuccess from '../assets/imgs/img_vote_success.png';
 import Button from './Button';
 import Modal from './Modal';
 
 function VoteSuccessModal({ open, onClose }) {
   return (
     <Modal open={open} onClose={onClose}>
-      <img src={ImgVoteSuccess} className="h-44 w-44" alt="투표 성공" />
+      <img src={imgVoteSuccess} className="my-6 h-44 w-44" alt="투표 성공" />
       <p className="mb-8 font-medium">
         투표가 <span className="text-pointOrange">성공</span>
         했어요!
