ce2bfdc6f64b63da647012b76ed1039827241c1a
kimhyesun <hyesun1004.kim@gmail.com>
2024-05-15 14:35:11 +0900

    💄 ui: 그룹명, 아이돌 이름 가운데 정렬

--- src/components/IdolThumbnail.jsx
+++ src/components/IdolThumbnail.jsx
@@ -10,7 +10,7 @@ function IdolThumbnail({ checked = false, group, name, src, size }) {
         <IdolAvatar alt={name} src={src} size={size} />
       )}
 
-      <dl className="mobile: mt-1.5 text-center mobile:w-[85px] tablet:w-[100px] desktop:w-[102px]">
+      <dl className="mobile: mx-auto mt-1.5 text-center mobile:w-[85px] tablet:w-[100px] desktop:w-[102px]">
         <dt className="text-base">{name}</dt>
         <dd className="mt-0.5 overflow-hidden text-ellipsis whitespace-nowrap break-all text-sm opacity-60">
           {group}
